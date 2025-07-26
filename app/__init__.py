3b998401023c04b6d19af4aca32b5c11c1044b7e
Lucas-G-B-Lima <lgabrielblima@gmail.com>
2025-07-25 23:45:44 -0300

    Configura PostgreSQL e ajusta DATABASE_URI

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 app = Flask(__name__, static_folder='static') 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 #app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
